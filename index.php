f4cebd9119f4a46780f14e51ef00b6b7139cf942
uxmansarwar <uxmansarwar@yahoo.com>
2023-05-10 01:55:45 +0500

    init

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+use uxmansarwar\DataTable;
+
+require_once 'vendor/autoload.php';
+
+// $class = new \uxmansarwar\DataTable();
+
+
+$cls = new DataTable();
---
+++ src/DataTable.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace uxmansarwar;
+
+
+class DataTable
+{
+
+
+
+    public function __construct()
+    {
+        return 'im working';
+    }
+}
