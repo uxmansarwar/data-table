5c300b8d5d4009a89e35351a8d4b54e74d867501
uxmansarwar <uxmansarwar@yahoo.com>
2023-05-12 00:15:33 +0500

    testing again

--- src/DataTable.php
+++ src/DataTable.php
@@ -6,10 +6,21 @@ namespace uxmansarwar;
 class DataTable
 {
 
+    public string $table;
 
 
     public function __construct()
     {
         return 'im working';
+    }
+
+    public function setTR()
+    {
+        return 'this is testing tr';
+    }
+
+    public function __toString()
+    {
+        return $this->table;
     }
 }
